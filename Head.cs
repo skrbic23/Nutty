d7d8aedf4292acce71197a373e7c2f70bd54b0a4
skrbic23 <63725782+skrbic23@users.noreply.github.com>
2021-04-09 14:55:43 +0200

    Kasnije cu se prebacim na formu

---
+++ Head.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nutty
+{
+    class Head
+    {
+        public const char blank = '_';
+
+        public Head()
+        {
+
+        }
+        public List<char> Tape { get; }
+        public int HeadPosition { get; }
+
+        public void HeadWrite(char head)
+        {
+
+        }
+        public void  Movehead(string direction)
+        {
+
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Nutty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //glava- head
+            //Traka - tape
+            //par stejtova - 4 cu da stavim
+
+        }
+    }
+}
