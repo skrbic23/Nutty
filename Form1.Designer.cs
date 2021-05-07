a7fc51f0aef6529bb124de4badc28f19f0003f4b
skrbic23 <63725782+skrbic23@users.noreply.github.com>
2021-05-07 14:57:14 +0200

    Pocetak

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace Nutty2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Sto1 = new System.Windows.Forms.Button();
+            this.Sto2 = new System.Windows.Forms.Button();
+            this.Sto3 = new System.Windows.Forms.Button();
+            this.Sto4 = new System.Windows.Forms.Button();
+            this.Sto5 = new System.Windows.Forms.Button();
+            this.Sto6 = new System.Windows.Forms.Button();
+            this.Sto7 = new System.Windows.Forms.Button();
+            this.Sto8 = new System.Windows.Forms.Button();
+            this.Sto9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Sto1
+            // 
+            this.Sto1.Location = new System.Drawing.Point(230, 32);
+            this.Sto1.Name = "Sto1";
+            this.Sto1.Size = new System.Drawing.Size(71, 61);
+            this.Sto1.TabIndex = 0;
+            this.Sto1.Text = "1";
+            this.Sto1.UseVisualStyleBackColor = true;
+            this.Sto1.Click += new System.EventHandler(this.Sto1_Click);
+            // 
+            // Sto2
+            // 
+            this.Sto2.Location = new System.Drawing.Point(422, 32);
+            this.Sto2.Name = "Sto2";
+            this.Sto2.Size = new System.Drawing.Size(70, 61);
+            this.Sto2.TabIndex = 1;
+            this.Sto2.Text = "2";
+            this.Sto2.UseVisualStyleBackColor = true;
+            // 
+            // Sto3
+            // 
+            this.Sto3.Location = new System.Drawing.Point(612, 32);
+            this.Sto3.Name = "Sto3";
+            this.Sto3.Size = new System.Drawing.Size(66, 61);
+            this.Sto3.TabIndex = 2;
+            this.Sto3.Text = "3";
+            this.Sto3.UseVisualStyleBackColor = true;
+            // 
+            // Sto4
+            // 
+            this.Sto4.Location = new System.Drawing.Point(33, 203);
+            this.Sto4.Name = "Sto4";
+            this.Sto4.Size = new System.Drawing.Size(84, 63);
+            this.Sto4.TabIndex = 3;
+            this.Sto4.Text = "4";
+            this.Sto4.UseVisualStyleBackColor = true;
+            // 
+            // Sto5
+            // 
+            this.Sto5.Location = new System.Drawing.Point(33, 340);
+            this.Sto5.Name = "Sto5";
+            this.Sto5.Size = new System.Drawing.Size(84, 57);
+            this.Sto5.TabIndex = 4;
+            this.Sto5.Text = "5";
+            this.Sto5.UseVisualStyleBackColor = true;
+            // 
+            // Sto6
+            // 
+            this.Sto6.Location = new System.Drawing.Point(230, 203);
+            this.Sto6.Name = "Sto6";
+            this.Sto6.Size = new System.Drawing.Size(158, 63);
+            this.Sto6.TabIndex = 5;
+            this.Sto6.Text = "6";
+            this.Sto6.UseVisualStyleBackColor = true;
+            // 
+            // Sto7
+            // 
+            this.Sto7.Location = new System.Drawing.Point(230, 334);
+            this.Sto7.Name = "Sto7";
+            this.Sto7.Size = new System.Drawing.Size(158, 63);
+            this.Sto7.TabIndex = 6;
+            this.Sto7.Text = "7";
+            this.Sto7.UseVisualStyleBackColor = true;
+            // 
+            // Sto8
+            // 
+            this.Sto8.Location = new System.Drawing.Point(538, 203);
+            this.Sto8.Name = "Sto8";
+            this.Sto8.Size = new System.Drawing.Size(158, 63);
+            this.Sto8.TabIndex = 7;
+            this.Sto8.Text = "8";
+            this.Sto8.UseVisualStyleBackColor = true;
+            // 
+            // Sto9
+            // 
+            this.Sto9.Location = new System.Drawing.Point(538, 327);
+            this.Sto9.Name = "Sto9";
+            this.Sto9.Size = new System.Drawing.Size(158, 63);
+            this.Sto9.TabIndex = 8;
+            this.Sto9.Text = "9";
+            this.Sto9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 405);
+            this.Controls.Add(this.Sto9);
+            this.Controls.Add(this.Sto8);
+            this.Controls.Add(this.Sto7);
+            this.Controls.Add(this.Sto6);
+            this.Controls.Add(this.Sto5);
+            this.Controls.Add(this.Sto4);
+            this.Controls.Add(this.Sto3);
+            this.Controls.Add(this.Sto2);
+            this.Controls.Add(this.Sto1);
+            this.Name = "Form1";
+            this.Text = "Kafic";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Sto1;
+        private System.Windows.Forms.Button Sto2;
+        private System.Windows.Forms.Button Sto3;
+        private System.Windows.Forms.Button Sto4;
+        private System.Windows.Forms.Button Sto5;
+        private System.Windows.Forms.Button Sto6;
+        private System.Windows.Forms.Button Sto7;
+        private System.Windows.Forms.Button Sto8;
+        private System.Windows.Forms.Button Sto9;
+    }
+}
+
---
+++ Form2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nutty2._0
+{
+    public partial class Racun : Form
+    {
+        public Racun()
+        {
+            InitializeComponent();
+        }
+    }
+}
